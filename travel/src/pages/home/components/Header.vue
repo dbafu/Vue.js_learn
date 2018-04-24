5a9b063affd7e2fbc01a98a0eb959f14fe02251b
dbafu <damon.fu@qq.com>
2018-04-24 23:51:45 +0800

    add city-router

--- travel/src/pages/home/components/Header.vue
+++ travel/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+          {{this.city}}
+          <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
+    </router-link>
   </div>
 </template>
 
@@ -28,7 +30,7 @@ export default {
 
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHight
     background: $bgColor
     .header-left
       width: .64rem
