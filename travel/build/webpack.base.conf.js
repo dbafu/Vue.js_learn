d5975481548c042c879c228c135ced72aff9058d
dbafu <damon.fu@qq.com>
2018-05-02 10:26:10 +0800

    banner finish

--- travel/build/webpack.base.conf.js
+++ travel/build/webpack.base.conf.js
@@ -38,6 +38,7 @@ module.exports = {
       '@': resolve('src'),
       'styles': resolve('src/assets/styles'),
       'img': resolve('src/assets/img'),
+      'common': resolve('src/common')
     }
   },
   module: {
---
+++ travel/src/pages/detail/components/Banner.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1508/a5/4003f9dd7bebf61eccbf64046e26d487.water.jpg_600x330_a80ff353.jpg"/>
+      <div class="banner-info">
+        <div class="banner-title">
+          北京欢乐谷(AAAA景区)
+        </div>
+        <div class="banner-num">
+          <span class="iconfont banner-icon">&#xe632;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary
+      :imgs="imgs"
+      v-show="showGallary"
+      @close="handleGallaryClose"
+    ></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1804/c8/c88bb5e69c9a9c5ea3.img.jpg_r_800x800_0a55341c.jpg', 'http://img1.qunarzz.com/sight/p0/1804/78/781d085517106ccea3.img.jpg_r_800x800_ae5b774c.jpg']
+    }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height 0.6rem
+      color #fff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+      .banner-title
+        flex 1
+        font-size 0.32rem
+        padding 0 0.2rem
+      .banner-num
+        height 0.32rem
+        line-height 0.32rem
+        margin-top 0.14rem
+        padding 0 0.4rem
+        border-radius 0.2rem
+        background rgba(0, 0, 0, .8)
+        font-size 0.24rem
+</style>
